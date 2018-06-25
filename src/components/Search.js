26068e8deca8f0fca836088ccf8e1455977be6d0
earthfor <earthfor@outlook.com>
2018-06-25 18:34:57 +0800

    Fixing search components arrow up and down problem, NOT Completed!

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,6 +4,13 @@ import PropTypes from 'prop-types'
 import './Search.css'
 
 class Search extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      value: props.value
+    }
+  }
   static get propTypes () {
     return {
       value: PropTypes.string,
@@ -12,13 +19,28 @@ class Search extends Component {
       autoFocus: PropTypes.bool
     }
   }
+  changeValue (value) {
+    this.setState({ value })
+  }
   render () {
     return (
       <div className='search-container'>
         <div className='search-item search-item-input'>
-          <input value={this.props.value} autoFocus={this.props.autoFocus} className='search-item-input-content' type='text' name='search' placeholder='请输入小说名称，作者，主角' autoComplete='off'
-            onChange={(e) => this.props.change(e.target.value)}
-            onKeyDown={(e) => this.props.keyDown(e)}
+          <input value={this.state.value} autoFocus={this.props.autoFocus} className='search-item-input-content' type='text' name='search' placeholder='请输入小说名称，作者，主角' autoComplete='off'
+            onChange={
+              (e) => {
+                const { value } = e.target
+                this.changeValue(value) // Change value immediately
+                this.props.change(value)
+              }
+            }
+            onKeyDown={
+              (e) => {
+                const value = this.props.keyDown(e)
+                console.log(value)
+                value !== undefined && this.changeValue(value)
+              }
+            }
           />
         </div>
         <div className='search-item-btn-container'>
--- src/page/Index.js
+++ src/page/Index.js
@@ -148,6 +148,7 @@ class Index extends Component {
       default:
         break
     }
+    return searchV
   }
 
   storeHistory (histories) {
